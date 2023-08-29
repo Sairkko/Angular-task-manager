64334e4ae024976c4b0494593c79e52eb693fc07
OEUVRARD Bastien <78555495+Sairkko@users.noreply.github.com>
2023-08-29 08:34:11 +0200

    Update by-statut-task-list-service.service.ts

--- src/app/services/by-statut-task-list-service.service.ts
+++ src/app/services/by-statut-task-list-service.service.ts
@@ -6,11 +6,7 @@ import {ByStatutTaskService} from "./by-statut-task-service";
 @Injectable({
   providedIn: 'root'
 })
-<<<<<<< HEAD
 export class ByStatutTaskListService extends CRUDTaskListService implements ByStatutTaskService {
-=======
-export class ByStatutTaskListService extends CRUDTaskListService {
->>>>>>> b39567d0c1a99166ad83e955c527da8a5845a0c9
   getTasksByStatus(): Task[] {
     return this.readTasks().filter(task => task.state === TaskState.TERMINEE);
   }
